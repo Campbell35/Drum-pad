708c4b3b1cf7b093c0290717bb1a2f1122abcab3
Campbell M <campbell.mason@hotmail.co.nz>
2022-03-22 10:18:05 +1300

    ready to deploy

---
+++ app.js
@@ -0,0 +1,55 @@
+const drums = new Howl({
+    "src": [
+     "drums/drums_drums.webm",
+     "drums/drums_drums.mp3"
+    ],
+    "sprite": {
+     "clap": [
+       0,
+       734.2630385487529
+     ],
+     "closed-hihat": [
+       2000,
+       445.94104308390035
+     ],
+     "crash": [
+       4000,
+       1978.6848072562354
+     ],
+     "kick": [
+       7000,
+       553.0839002267571
+     ],
+     "open-hihat": [
+        9000,
+        962.7664399092968
+     ],
+     "snare": [
+        11000,
+        354.48979591836684
+     ]
+    
+    }
+      
+});
+
+const drumkit = document.querySelector('.drumkit');
+function playDrum(event) {
+    if (event.target.classList.contains('pad')) {
+        event.preventDefault();
+        let soundToPlay = event.target.dataset.sound;
+        drums.play(soundToPlay);
+    }
+}
+
+function setViewportHeight() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('==vh', `${vh}px`);
+}
+
+setViewportHeight();
+window.addEventListener('resize', () => {
+    setTimeout(setViewportHeight, 100);
+});
+drumkit.addEventListener('click', playDrum);
+drumkit.addEventListener('touchstart', playDrum);
